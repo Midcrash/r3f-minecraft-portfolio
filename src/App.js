d87774ac99973810a34a13c0ee6b16bd762300cf
Tyrae Yao <yaotyrae@gmail.com>
2022-05-15 01:47:15 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./assets/styles/App.scss";
+import img from "./assets/imgs/Tyrae-Yao.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg"></div>
+      <div className="main-container">
+        <div className="title-image-container">
+          <img src={img} alt="Top Text" className="title-image" />
+        </div>
+        <div className="main-text-container">
+          <div className="main-text">
+            <p className="title-text">Start Portfolio</p>
+          </div>
+          <div className="main-text">
+            <p className="title-text">Contact Me</p>
+          </div>
+          <div className="main-text">
+            <p className="title-text">???</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-container">
+        <div className="footer-text-container">
+          <p className="footer-text">Portfolio 1.0.0</p>
+          <p className="footer-text">
+            Powered by Love, Passion, and Motivation
+          </p>
+        </div>
+        <div className="footer-text-right">
+          <p className="footer-text">Made by Tyrae Yao :)</p>
+        </div>
+      </div>
     </div>
   );
 }
