b6867395f2dd327b42875ef212c7dfabab85ceee
Tyrae Yao <yaotyrae@gmail.com>
2022-07-24 17:46:18 -0700

    Updated main page and project modal window

--- src/components/modalProjects.js
+++ src/components/modalProjects.js
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "../assets/imgs/logo.png";
+import MCLogo from "../assets/imgs/MCLogo.png";
 
 const ModalProjects = (props) => {
   if (!props.open1) {
@@ -13,9 +15,52 @@ const ModalProjects = (props) => {
         <div className="modal">
           <div className="project-container">
             <div className="project-items">
-              <p className="project-item">Empty 1</p>
-              <p className="project-item">Empty 2</p>
-              <p className="project-item">Empty 3</p>
+              <div className="item-container">
+                <a
+                  className="project-item"
+                  href="https://github.com/Midcrash/Inventory-Hero"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img alt="logo" src={logo} className="item-logo" />
+                  <div>
+                    <p className="item-title">Inventory Hero</p>
+                    <p className="item-desc">
+                      Full Stack Application - AWS Relational Database, PHP,
+                      Bootstrap{" "}
+                    </p>
+                  </div>
+                </a>
+                <a
+                  className="project-item"
+                  href="https://github.com/Midcrash/r3f-minecraft-portfolio"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img alt="MClogo" src={MCLogo} className="item-logo" />
+                  <div>
+                    <p className="item-title">Square Dashboard</p>
+                    <p className="item-desc">
+                      Full Stack Application - Next.Js, React, Firebase, Square
+                      API, TailwindCSS
+                    </p>
+                  </div>
+                </a>
+                <a
+                  className="project-item"
+                  href="https://github.com/Midcrash/r3f-minecraft-portfolio"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img alt="MClogo" src={MCLogo} className="item-logo" />
+                  <div>
+                    <p className="item-title">Minecraft Portfolio</p>
+                    <p className="item-desc">
+                      Front End Application - React, R3F, Drei, React Router
+                    </p>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
